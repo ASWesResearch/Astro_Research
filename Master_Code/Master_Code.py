15223eafd351799c7f1b79af841640bf9a33d40b
Anthony Santini <asantini@wesleyan.edu>
2018-06-19 09:41:37 -0400

    Master Output, .DS_Store, .pyc  and Master Code Modifcations (Multithreading Almost Working)

--- Master_Code/Master_Code.py
+++ Master_Code/Master_Code.py
@@ -112,11 +112,13 @@ def Pipeline_B(Gname_L):
                 Cur_Fov_Filepath=Fov_File_L[1]
                 if(Cur_Evt2_ObsID==Cur_Fov_ObsID):
                     #print "Test"
-                    path_Obs=path_Area+'/'+str(Cur_Evt2_ObsID)+'/'
+                    #path_Obs=path_Area+'/'+str(Cur_Evt2_ObsID)+'/'
+                    path_Obs=path_Area+str(Cur_Evt2_ObsID)+'/'
                     directory_Obs=os.path.dirname(path_Obs)
                     if not os.path.exists(directory_Obs):
                         os.makedirs(directory_Obs)
-                    os.chdir(path_Obs)
+                    #os.chdir(path_Obs) #This os.chdir actullay effects the outcome of the code
+                    #print "path_Obs : ", path_Obs
                     #print "THE PWD AT END IS :"
                     #system('pwd')
                     #print "CURRENT FOV FILEPATH IS :", Cur_Fov_Filepath
@@ -132,27 +134,30 @@ def Pipeline_B(Gname_L):
                     #print "Cur_Fov_Filename_Ext_L : ",Cur_Fov_Filename_Ext_L
                     Cur_Fov_Filename_Ext=Cur_Fov_Filename_Ext_L[len(Cur_Fov_Filename_Ext_L)-1]
                     #print "Cur_Fov_Filename_Ext : ", Cur_Fov_Filename_Ext
-                    if(Cur_Fov_Filename_Ext=="gz"):
+                    if(Cur_Fov_Filename_Ext=="gz"): #Need to test this later with a compressed fits file
                         Output_Fov_Filename=Cur_Fov_Filename_Ext_L[0]+"."+Cur_Fov_Filename_Ext_L[1]
                         #os.chdir(path_Obs)
-                        os.chdir(Cur_Fov_Filepath_No_Fname)
+                        #Cur_Fov_Filepath
+                        #os.chdir(Cur_Fov_Filepath_No_Fname)
                         #system('pwd')
                         Output_Fov_Filepath=Cur_Fov_Filepath_No_Fname+"/"+Output_Fov_Filename
                         #print "Output_Fov_Filepath : ",Output_Fov_Filepath
-                        inF = gzip.open(Cur_Fov_Filename, 'rb')
-                        outF = open(Output_Fov_Filename, 'wb')
+                        #inF = gzip.open(Cur_Fov_Filename, 'rb')
+                        #outF = open(Output_Fov_Filename, 'wb')
+                        inF = gzip.open(Cur_Fov_Filepath, 'rb')
+                        outF = open(Output_Fov_Filepath, 'wb')
                         outF.write( inF.read() )
                         inF.close()
                         outF.close()
-                        os.chdir(path_Obs)
+                        #os.chdir(path_Obs)
                         Cur_Fov_Filepath=Output_Fov_Filepath
                     #print "Cur_Evt2_Filepath : ", Cur_Evt2_Filepath
                     #print "Cur_Fov_Filepath : ", Cur_Fov_Filepath
-                    XPA_DS9_Region_Generator_3.XPA_DS9_Region_Generator(Cur_Evt2_Filepath,Cur_Fov_Filepath)
+                    XPA_DS9_Region_Generator_3.XPA_DS9_Region_Generator(Cur_Evt2_Filepath,Cur_Fov_Filepath,path_Obs) #Need to update file output location
                     #print "The Filepath Before End is : "
                     #system('pwd')
                     #time.sleep(10.0)
-                    Region_File_LS= os.popen("ls").read()
+                    Region_File_LS= os.popen("ls "+path_Obs).read()
                     #print "Region_File_LS : ", Region_File_LS
                     Region_File_LS_L=Region_File_LS.split("\n")
                     #print "Region_File_LS_L : ",Region_File_LS_L
@@ -165,13 +170,13 @@ def Pipeline_B(Gname_L):
                             Region_Filename=Region_Filename_Test
                     #print "Region_Filename : ",Region_Filename
                     #This_is_Ment_To_Break_The_Code
-                    Simple_Region_Generator_9.Simple_Region_Generator(Region_Filename,Cur_Evt2_Filepath)
+                    Simple_Region_Generator_9.Simple_Region_Generator(Region_Filename,Cur_Evt2_Filepath,path_Obs) #Need to update file output location
                     #print "Cur_Evt2_Filepath: ", Cur_Evt2_Filepath
                     #Area_Calc_Frac_B_2_Alt_2("NGC 253","/Network/Servers/vimes.astro.wesleyan.edu/Volumes/vvodata/home/asantini/Desktop/CCD_Incompleteness_Correction/Area_Calc/acisf13830_repro_evt2.fits","acisf13830_repro_CCD_Regions_simple_region_modifed_Code.txt")
                     #print "The Filepath Before LS Delay"
                     #system('pwd')
                     #time.sleep(10.0)
-                    Simple_Region_File_LS= os.popen("ls").read()
+                    Simple_Region_File_LS= os.popen("ls "+path_Obs).read()
                     #print "Simple_Region_File_LS : ", Simple_Region_File_LS
                     #print "The Filepath Before LS Delay"
                     #system('pwd')
@@ -185,15 +190,18 @@ def Pipeline_B(Gname_L):
                         if(("modifed" in Simple_Region_Filename_Test_L) and ("no" not in Simple_Region_Filename_Test_L)):
                             Simple_Region_Filename=Simple_Region_Filename_Test
                     #print "Simple_Region_Filename : ",Simple_Region_Filename
+                    Simple_Region_Filepath=path_Obs+Simple_Region_Filename
+                    #print "Simple_Region_Filepath : ", Simple_Region_Filepath
                     #Area_Calc_Frac_B_2_Alt_8.Area_Calc_Frac_B_2_Alt_2(Gname,Simple_Region_Filename,Cur_Evt2_Filepath)
-                    Cur_Area_L=Area_Calc_Frac_B_2_Alt_8.Area_Calc_Frac_B_2_Alt_2(Gname,Cur_Evt2_Filepath,Simple_Region_Filename)
+                    #Cur_Area_L=Area_Calc_Frac_B_2_Alt_8.Area_Calc_Frac_B_2_Alt_2(Gname,Cur_Evt2_Filepath,Simple_Region_Filename)
+                    Cur_Area_L=Area_Calc_Frac_B_2_Alt_8.Area_Calc_Frac_B_2_Alt_2(Gname,Cur_Evt2_Filepath,Simple_Region_Filepath)
                     #print Cur_Area_L
                     #system('pwd')
-                    Simple_Region_Filename_L=Simple_Region_Filename.split("CCD")
+                    #Simple_Region_Filename_L=Simple_Region_Filename.split("CCD")
                     #print "Simple_Region_Filename_L : ",Simple_Region_Filename_L
-                    Simple_Region_Filename_Reduced=Simple_Region_Filename_L[0]
+                    #Simple_Region_Filename_Reduced=Simple_Region_Filename_L[0]
                     #print "Simple_Region_Filename_Reduced : ",Simple_Region_Filename_Reduced
-                    Area_List_Filename=Simple_Region_Filename_Reduced+"Area_List.txt"
+                    #Area_List_Filename=Simple_Region_Filename_Reduced+"Area_List.txt"
                     #print "Area_List_Filename : ",Area_List_Filename
                     #print "fovfname_reduced : ",fovfname_reduced
                     #print "Cur_Evt2_Filepath : ",Cur_Evt2_Filepath
@@ -215,9 +223,9 @@ def Pipeline_B(Gname_L):
                     """
                     #print "PWD before Path_5 : "
                     #system('pwd')
-        path_5=os.path.realpath('../../../../')
+        #path_5=os.path.realpath('../../../../')
         #print "path_5 : ",path_5
-        os.chdir(path_5)
+        #os.chdir(path_5)
         #print "THE PWD AT VERY END IS :"
         #system('pwd')
         #Need to add the rest of Pipeline B after XPA_DS9_Region_Generator
@@ -240,26 +248,12 @@ def Pipeline_C(Gname_L):
     #print sys.path
     #Import Desktop Modules Here !!!
     from File_Query_Code import File_Query_Code_5
-    #print "PWD C 2a : "
-    #system('pwd')
     from Source_Region_Generator import Source_Region_Generator_Radius_Modifed_V3
-    #print "PWD C 2b : "
-    #system('pwd')
     from Background_Finder import Background_Finder_10
-    #print "PWD C 2c : "
-    #system('pwd')
     from Detection_Probablity_Calc import Detection_Probability_Calc_7
-    #print "PWD C 2d : "
-    #system('pwd')
     from Known_Flux_Finder import Known_Flux_Finder
-    #print "PWD C 2e : "
-    #system('pwd')
     from Counts_To_Flux_Converter import Counts_To_Flux_Converter_3
-    #print "PWD C 2f : "
-    #system('pwd')
     from Galaxy_Name_Reducer import Galaxy_Name_Reducer
-    #print "PWD C 2g : "
-    #system('pwd')
     for Gname in Gname_L:
         #Pipeline_C Code
         """
@@ -315,7 +309,8 @@ def Pipeline_C(Gname_L):
                     directory_Obs=os.path.dirname(path_Obs)
                     if not os.path.exists(directory_Obs):
                         os.makedirs(directory_Obs)
-                    os.chdir(path_Obs)
+                    #print "path_Obs : ", path_Obs
+                    #os.chdir(path_Obs)
                     #Cur_Evt2_Filepath_Full=path_Obs+Cur_Evt2_Filename
                     #print "path_Obs : ", path_Obs
                     #print "Cur_Evt2_Filepath_Full : ", Cur_Evt2_Filepath_Full
@@ -331,10 +326,12 @@ def Pipeline_C(Gname_L):
                     #print "Gname_Underscore Master : ", Gname_Underscore
                     #NGC_4258_ObsID_1618_Source_Regions_Radius_Modifed.txt
                     ObjF_Filename=Gname_Underscore+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Source_Regions_Radius_Modifed.txt"
+                    ObjF_Filepath=path_Obs+ObjF_Filename
+                    #print "ObjF_Filepath : ", ObjF_Filepath
                     BG_Radius=50
                     while (BG_Radius>20):
                         #Background_Finder_3(gname,evtfpath,objLfname,R)
-                        Background_Ratio=Background_Finder_10.Background_Finder_3(Gname,Cur_Evt2_Filepath,ObjF_Filename,BG_Radius)
+                        Background_Ratio=Background_Finder_10.Background_Finder_3(Gname,Cur_Evt2_Filepath,ObjF_Filepath,BG_Radius)
                         if(Background_Ratio != "None_Found"):
                             break
                         BG_Radius=BG_Radius-10
@@ -343,10 +340,11 @@ def Pipeline_C(Gname_L):
                     #D_P_C_Big_Input_90_Per_Check(Backgrounds,Off_Angs,C_Min=2,C_Max=110)
                     #print "THE PWD AT END IS :"
                     #system('pwd')
-                    path_4=os.path.realpath('../../../../')
+                    #path_4=os.path.realpath('../../../../')
                     #print "path_4 : ",path_4
-                    os.chdir(path_4)
+                    #os.chdir(path_4)
                     #print "Output : ",Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
+                    #system('pwd')
                     C_90_Per_First_L_H=Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
                     #print "C_90_Per_First_L_H : ", C_90_Per_First_L_H
                     #os.chdir(path_Obs)
@@ -361,19 +359,19 @@ def Pipeline_C(Gname_L):
                     #print "Flux_90_L_H : ", Flux_90_L_H
                     #print "THE PWD AT END IS :"
                     #system('pwd')
-                    os.chdir(path_Obs)
+                    #os.chdir(path_Obs)
                     #print "THE PWD AT END IS :"
                     #system('pwd')
                     Flux_90_L=Flux_90_L_H[0]
                     #print "Flux_90_L : ", Flux_90_L
                     #NGC_4258_ObsID_1618
-                    file=open(Gname_Modifed+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Flux_90.txt","w")
+                    file=open(path_Obs+Gname_Modifed+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Flux_90.txt","w")
                     file.write("Background_Ratio:"+str(Background_Ratio)+"|"+"\n")
                     for Flux_90 in Flux_90_L:
                         file.write(str(Flux_90)+"\n")
-        path_5=os.path.realpath('../../../../')
+        #path_5=os.path.realpath('../../../../')
         #print "path_5 : ",path_5
-        os.chdir(path_5)
+        #os.chdir(path_5)
         #print "THE PWD AT VERY END IS :"
         #system('pwd')
 
@@ -423,13 +421,14 @@ def Pipeline_D(Gname_L):
                     directory_Obs=os.path.dirname(path_Obs)
                     if not os.path.exists(directory_Obs):
                         os.makedirs(directory_Obs)
-                    os.chdir(path_Obs)
+                    #print "path_Obs : ", path_Obs
+                    #os.chdir(path_Obs)
                     #Cur_Evt2_Filepath_Full=path_Obs+Cur_Evt2_Filename
                     #print "path_Obs : ", path_Obs
                     Source_C_L=Coords_Calc.Coords_Calc(Cur_Evt2_Filepath,Cur_Reg_Filepath)
                     #print "PWD AT THE END : "
                     #system('pwd')
-                    file2=open(Gname_Modifed+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Coords.csv","w")
+                    file2=open(path_Obs+Gname_Modifed+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Coords.csv","w")
                     #[Cur_X,Cur_Y,Cur_Chip_X,Cur_Chip_Y,Cur_Chip_ID,Cur_RA,Cur_DEC,Cur_Theta]
                     file2.write("Phys_X,Phys_Y,Chip_X,Chip_Y,Chip_ID,RA,DEC,Offaxis_Angle"+"\n")
                     for Source_C in Source_C_L:
@@ -442,35 +441,35 @@ def Pipeline_D(Gname_L):
                         DEC=Source_C[6]
                         Offaxis_Angle=Source_C[7]
                         file2.write(str(Phys_X)+","+str(Phys_Y)+","+str(Chip_X)+","+str(Chip_Y)+","+str(Chip_ID)+","+str(RA)+","+str(DEC)+","+str(Offaxis_Angle)+"\n")
-        path_5=os.path.realpath('../../../../')
+        #path_5=os.path.realpath('../../../../')
         #print "path_5 : ",path_5
-        os.chdir(path_5)
+        #os.chdir(path_5)
         #print "THE PWD AT VERY END IS :"
         #system('pwd')
-
-def Master(Gname_L):
-    #Thread(target = Pipeline_A(Gname_L)).start()
-    Thread(target = Pipeline_B(Gname_L)).start()
-    #Thread(target = Pipeline_C(Gname_L)).start()
-    #Thread(target = Pipeline_D(Gname_L)).start()
-    print "Number of Threads : ", str(threading.activeCount())
-    print "Master Complete"
-
 """
 def Master(Gname_L):
-    A=Thread(target = Pipeline_A, args=(Gname_L))
-    B=Thread(target = Pipeline_B, args=(Gname_L))
-    C=Thread(target = Pipeline_C, args=(Gname_L))
-    D=Thread(target = Pipeline_D, args=(Gname_L))
+    Thread(target = Pipeline_A(Gname_L)).start()
+    Thread(target = Pipeline_B(Gname_L)).start()
+    Thread(target = Pipeline_C(Gname_L)).start()
+    Thread(target = Pipeline_D(Gname_L)).start()
+    print "Number of Threads : ", str(threading.activeCount())
+    print "Master Complete"
+"""
+
+def Master(Gname_L):
+    A=Thread(target = Pipeline_A, args=(Gname_L,))
+    B=Thread(target = Pipeline_B, args=(Gname_L,))
+    C=Thread(target = Pipeline_C, args=(Gname_L,))
+    D=Thread(target = Pipeline_D, args=(Gname_L,))
     A.start()
     B.start()
     C.start()
     D.start()
     print "Number of Threads : ", str(threading.activeCount())
     print "Master Complete"
-"""
+
 if __name__ == '__main__':
     #Master(['NGC4258','M31','NGC 1365']) #This works
     #Master(['NGC4258','NGC 1332'])
-    #Master(['NGC4258','NGC 1365'])
-    Master(['NGC4258'])
+    Master(['NGC4258','NGC 1365'])
+    #Master(['NGC4258'])
