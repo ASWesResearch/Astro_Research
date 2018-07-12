e4b059eaae5e6a61b07e1ce57c50a2a2672bd913
Anthony Santini <asantini@wesleyan.edu>
2018-07-12 13:32:22 -0400

    Modifed Counter.py in 2 ways  1. it now works with Source_Flux_Table.csv instead of SQL_Standard_File.csv and uses the filename Source_Flux_Table.csv as an input instead of hard coding the filename in the code itself 2. Counter.py now returns a list of all unqiue resloved galaxy names ('resolvedObject'), that means all 141 galaxy names are listed once each in a list. This is used as the input to the Master_Code

--- Counter/Counter.py
+++ Counter/Counter.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
-def Counter():
-    SQL_Data=pd.read_csv("SQL_Standard_File.csv")
-    Galaxy_Name_Data=pd.read_csv("Galaxy_List.csv")
-    Galaxy_Name_Data_L=list(Galaxy_Name_Data['queriedName'])
-    print "Galaxy_Name_Data_L : ", Galaxy_Name_Data_L
+import os
+from os import system
+def Counter(fname):
+    #SQL_Data=pd.read_csv("SQL_Standard_File.csv")
+    fpath=os.path.realpath('../SQL_Standard_File/'+fname)
+    #print "fpath : ", fpath
+    SQL_Data=pd.read_csv(fpath)
+    #Galaxy_Name_Data=pd.read_csv("Galaxy_List.csv")
+    #Galaxy_Name_Data_L=list(Galaxy_Name_Data['queriedName'])
+    #print "Galaxy_Name_Data_L : ", Galaxy_Name_Data_L
     #print SQL_Data
     #SQL_Data_L=list(SQL_Data)
     #print (SQL_Data_L)
@@ -19,23 +24,34 @@ def Counter():
     Num_Sources=Num_Rows
     print "Num_Sources : ", Num_Sources
     #obsid
-    Obs_ID_Table=SQL_Data['obsid']
+    #Obs_ID_Table=SQL_Data['obsid']
+    #OBSID
+    Obs_ID_Table=SQL_Data['OBSID']
     #print "Obs_ID_Table : ", Obs_ID_Table
     #Obs_ID_L=list(Obs_ID_Table)
     #print "Obs_ID_L : ", Obs_ID_L
     #for Obs_ID in Obs_ID_L:
-    Obs_ID_Table_Grouped=SQL_Data.groupby('obsid')['queriedName'].nunique()
+    #resolvedObject
+    Obs_ID_Table_Grouped=SQL_Data.groupby('OBSID')['resolvedObject'].nunique()
     #print "Obs_ID_Table_Grouped : ", Obs_ID_Table_Grouped
     Obs_ID_Table_Grouped_Shape=Obs_ID_Table_Grouped.shape
     Num_ObsIDs=Obs_ID_Table_Grouped_Shape[0]
     print "Num_ObsIDs : ", Num_ObsIDs
     #print "type(Num_ObsIDs) : ", type(Num_ObsIDs)
-    Gname_Table_Grouped=SQL_Data.groupby('queriedName')['obsid'].nunique()
-    print Gname_Table_Grouped
+    Gname_Table_Grouped=SQL_Data.groupby('resolvedObject')['OBSID'].nunique()
+    #print Gname_Table_Grouped
     #Gname_A_Grouped=np.array(Gname_Table_Grouped)
     #print "Gname_A_Grouped : ", Gname_A_Grouped
     Gname_Table_Grouped_Shape=Gname_Table_Grouped.shape
     Num_Gname=Gname_Table_Grouped_Shape[0]
     print "Num_Gname : ", Num_Gname
     #print "type(Num_Gname) : ", type(Num_Gname)
-Counter()
+    Name_Table=SQL_Data['resolvedObject']
+    #print "Name_Table : \n", Name_Table
+    Name_L=list(Name_Table)
+    #print "Name_L : ", Name_L
+    Name_L_Unique=set(Name_L)
+    print "Name_L_Unique : ", Name_L_Unique
+    #print "len(Name_L_Unique) : ", len(Name_L_Unique)
+
+Counter("Source_Flux_Table.csv")
