2ef770a1298af17273b69bc6f8734f26c69f0562
Anthony Santini <asantini@wesleyan.edu>
2018-07-12 14:15:56 -0400

    Modifed Galaxy_Name_Reducer.py so that it removes whitespace in the begining and end of a galaxy name. For example, '   NGC 253  ' will be reduced to 'NGC_253'

--- Galaxy_Name_Reducer/Galaxy_Name_Reducer.py
+++ Galaxy_Name_Reducer/Galaxy_Name_Reducer.py
@@ -4,10 +4,24 @@ def Galaxy_Name_Reducer(Gname):
     """
     #print "Hello_World"
     Gname_R=Gname
+    i=0
+    while(Gname_R[i]==" "):
+        #print "Gname_R[i] : ", Gname_R[i]
+        i=i+1
+        #print "i : ", i
+    Gname_R=Gname_R[i:]
+    #print "Gname_R:", Gname_R
+    i=len(Gname_R)-1
+    while(Gname_R[i]==" "):
+        #print "Gname_R[i] : ", Gname_R[i]
+        i=i-1
+        #print "i : ", i
+    Gname_R=Gname_R[:i+1]
+    #print "Gname_R 2:", Gname_R
     #print "Gname : ",Gname
-    Space_Included_Bool=(" " in Gname)
+    Space_Included_Bool=(" " in Gname_R)
     #print "Space_Included_Bool : ", Space_Included_Bool
-    Underscore_Included_Bool=("_" in Gname)
+    Underscore_Included_Bool=("_" in Gname_R)
     #print "Underscore_Included_Bool : ", Underscore_Included_Bool
     if(Underscore_Included_Bool):
         Gname_R_L=Gname_R.split("_")
@@ -23,8 +37,8 @@ def Galaxy_Name_Reducer(Gname):
             #print "Gname_R_Right : ", Gname_R_Right
             Gname_R=Gname_R_Left+"_"+Gname_R_Right_No_0
         return Gname_R
-    if(" " in Gname):
-        Gname_R=Gname.replace(" ","_")
+    if(" " in Gname_R):
+        Gname_R=Gname_R.replace(" ","_")
         Gname_R_L=Gname_R.split("_")
         Gname_R_Left=Gname_R_L[0]
         #print "Gname_R_L : ", Gname_R_L
@@ -41,22 +55,22 @@ def Galaxy_Name_Reducer(Gname):
     elif(Underscore_Included_Bool==False):
         i=0
         j=0
-        #print "Gname : ", Gname
-        while(i<len(Gname)-1):
+        #print "Gname_R : ", Gname_R
+        while(i<len(Gname_R)-1):
             i=i+1
-            Cur_Sym=Gname[i]
-            Pre_Sym=Gname[j]
+            Cur_Sym=Gname_R[i]
+            Pre_Sym=Gname_R[j]
             #print "Cur_Sym : ", Cur_Sym
             #print "Pre_Sym : ", Pre_Sym
             Cur_Num_B=Cur_Sym.isdigit()
             Pre_Num_B=Pre_Sym.isdigit()
             #print "Cur_Num_B : ", Cur_Num_B
             #print "Pre_Num_B : ", Pre_Num_B
-            Gname_Length=len(Gname)
+            Gname_Length=len(Gname_R)
             #print "type(Gname_Length)", type(Gname_Length)
             if((Cur_Num_B==True) and(Pre_Num_B==False)):
-                Gname_Letters=Gname[0:i]
-                Gname_Numbers=Gname[i:Gname_Length]
+                Gname_Letters=Gname_R[0:i]
+                Gname_Numbers=Gname_R[i:Gname_Length]
                 #print "Gname_Letters : ", Gname_Letters
                 #print "Gname_Numbers : ", Gname_Numbers
                 if((Gname_Letters=="NGC") and (Gname_Numbers[0]=="0")):
@@ -84,4 +98,8 @@ def Galaxy_Name_Reducer(Gname):
 #print Galaxy_Name_Reducer("NGC0346")
 #print Galaxy_Name_Reducer("NGC_0346")
 #NGC 891
-#print Galaxy_Name_Reducer("NGC 891")
+#print Galaxy_Name_Reducer("    NGC 0346")
+#print Galaxy_Name_Reducer("NGC0346     ")
+#print Galaxy_Name_Reducer("       NGC_0346      ")
+#print Galaxy_Name_Reducer("       NGC0346      ")
+#print Galaxy_Name_Reducer("       NGC 0346      ")
