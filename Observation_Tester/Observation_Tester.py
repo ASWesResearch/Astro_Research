e7ddd95df7f0a2840e310d358d53359fbe599ca3
Anthony Santini <asantini@wesleyan.edu>
2019-07-02 15:09:21 -0400

    Added the functionality to only check the Max Exposure Observations

--- Observation_Tester/Observation_Tester.py
+++ Observation_Tester/Observation_Tester.py
@@ -11,7 +11,7 @@ print "path : ", path
 sys.path.append(os.path.abspath(path))
 from File_Query_Code import File_Query_Code_5
 from Galaxy_Name_Reducer import Galaxy_Name_Reducer
-def Observation_Tester(Gname_L,Simple_Reg_B=False):
+def Observation_Tester(Gname_L,Simple_Reg_B=False,Max_Exp_B=False):
     #d = pyds9.DS9()
     Accept_L=[]
     Reject_L=[]
@@ -20,17 +20,29 @@ def Observation_Tester(Gname_L,Simple_Reg_B=False):
     Galaxy_Counter=0
     Obs_ID_Counter=0
     Num_Galaxies=len(Gname_L)
+    #print "Testing ! ! !"
     for Gname in Gname_L:
         Galaxy_Counter=Galaxy_Counter+1
         Gname_Modifed=Galaxy_Name_Reducer.Galaxy_Name_Reducer(Gname)
+        print "Gname : ",Gname_Modifed
         G_Data = Ned.query_object(Gname) #G_Data:-astropy.table.table.Table, Galaxy_Data, The queryed data of the galaxy from NED in the form of a astropy table
         #print G_Data
         #print type(G_Data)
         raGC=float(G_Data['RA(deg)']) #raGC:-float, Right Ascension of Galatic Center, The right ascension of the galatic center of the current galaxy in degrees.
         decGC=float(G_Data['DEC(deg)']) #decGC:-float, Declination of Galatic Center, The declination of the galatic center of the current galaxy in degrees.
-        Evt2_File_H_L=File_Query_Code_5.File_Query(Gname,"evt2",Obs_Check_B=False)
-        Reg_File_H_L=File_Query_Code_5.File_Query(Gname,"reg",".reg",Obs_Check_B=False)
-        Fov_File_H_L=File_Query_Code_5.File_Query(Gname,"fov1",Obs_Check_B=False)
+        if(Max_Exp_B):
+            print "Test: True"
+            Evt2_File_H_L=File_Query_Code_5.File_Query(Gname,"evt2",Obs_Check_B=False,Exp_Max_B=True)
+            #print "Evt2_File_H_L : ", Evt2_File_H_L
+            Reg_File_H_L=File_Query_Code_5.File_Query(Gname,"reg",".reg",Obs_Check_B=False)
+            Fov_File_H_L=File_Query_Code_5.File_Query(Gname,"fov1",Obs_Check_B=False)
+        else:
+            print "TEST: False"
+            Evt2_File_H_L=File_Query_Code_5.File_Query(Gname,"evt2",Obs_Check_B=False)
+            Reg_File_H_L=File_Query_Code_5.File_Query(Gname,"reg",".reg",Obs_Check_B=False)
+            Fov_File_H_L=File_Query_Code_5.File_Query(Gname,"fov1",Obs_Check_B=False)
+        #/Volumes/xray/anthony/Thesis/Galaxy_Optical_Image_Query/Galaxy_Optical_Images/MESSIER_066
+        D25_Region_Path="/Volumes/xray/anthony/Thesis/Galaxy_Optical_Image_Query/Galaxy_Optical_Images/"+Gname_Modifed+"/"+Gname_Modifed+".reg"
         #print "Evt2_File_H_L ", Evt2_File_H_L
         #print "Fov_File_H_L ", Fov_File_H_L
         for Evt2_File_L in Evt2_File_H_L:
@@ -90,6 +102,7 @@ def Observation_Tester(Gname_L,Simple_Reg_B=False):
                             if(Simple_Reg_B==True):
                                 d.set("regions " + str(Simple_Region_Filepath))
                             d.set("regions " + str(Cur_Reg_Filepath))
+                            d.set("regions " + str(D25_Region_Path))
                             d.set("scale log")
                             d.set("bin buffersize 8192")
                             d.set("zoom to fit")
@@ -125,4 +138,26 @@ def Observation_Tester(Gname_L,Simple_Reg_B=False):
 
 
 #Observation_Tester(["NGC 253"])
-Observation_Tester(['NGC 4278', 'NGC 5204', 'NGC 2841', 'NGC 3877', 'MESSIER 106', 'NGC 5194', 'MESSIER 104', 'MESSIER 105', 'MESSIER 101', 'NGC 5054', 'NGC 5813', 'MESSIER 108', 'MESSIER 066', 'MESSIER 061', 'MESSIER 063', 'MESSIER 086', 'MESSIER 084', 'MESSIER 083', 'MESSIER 082', 'MESSIER 081', 'NGC 0278', 'MESSIER 088', 'NGC 1042', 'NGC 6744', 'IC 5332', 'NGC 3585', 'NGC 4478', 'NGC 7507', 'NGC 1637', 'NGC 4473', 'NGC 1365', 'MESSIER 074', 'NGC 4476', 'NGC 4570', 'NGC 5576', 'NGC 4321', 'NGC 5474', 'NGC 7090', 'MESSIER 094', 'MESSIER 095', 'NGC 4494', 'NGC 0247', 'NGC 4490', 'IC 1613', 'NGC 4477', 'NGC 4365', 'NGC 2787', 'NGC 3557', 'IC 5267', 'NGC 4388', 'NGC 3923', 'NGC 4945', 'NGC 891', 'NGC 1300', 'UGC 05340', 'NGC 3631', 'UGCA 166', 'NGC 4314', 'NGC 4550', 'Holmberg IX                   ', 'NGC 4559', 'IC 1459', 'NGC 1399', 'NGC 4039', 'NGC 4038', 'NGC 1316', 'NGC 1097', 'NGC 0383', 'NGC 2681', 'NGC 5018', 'NGC 5253', 'NGC 4631', 'NGC 4308', 'MESSIER 060', 'NGC 4742', 'NGC 1672', 'NGC 5846', 'NGC 4725', 'NGC 2403', 'NGC 3507', 'MESSIER 087', 'NGC 0891', 'NGC 4698', 'NGC 3384', 'NGC 6946', 'NGC 1291:[LFF2012] 084', 'NGC 3115', 'NGC 1332', 'NGC 1700', 'NGC 5584', 'NGC 4527', 'NGC 7552', 'NGC 2997', 'NGC 4449', 'MESSIER 049', 'NGC 3198', 'NGC 0855', 'NGC 7793', 'NGC 0119', 'NGC 2865', 'Circinus Galaxy               ', 'MESSIER 059', 'NGC 7331', 'NGC 1427', 'NGC 3628', 'NGC 3608', 'NGC 0055', 'NGC 4457', 'NGC 4214', 'NGC 4459', 'NGC 3521','NGC 4565', 'NGC 1313', 'NGC 0253'])
+#Observation_Tester(['NGC 4278', 'NGC 5204', 'NGC 2841', 'NGC 3877', 'MESSIER 106', 'NGC 5194', 'MESSIER 104', 'MESSIER 105', 'MESSIER 101', 'NGC 5054', 'NGC 5813', 'MESSIER 108', 'MESSIER 066', 'MESSIER 061', 'MESSIER 063', 'MESSIER 086', 'MESSIER 084', 'MESSIER 083', 'MESSIER 082', 'MESSIER 081', 'NGC 0278', 'MESSIER 088', 'NGC 1042', 'NGC 6744', 'IC 5332', 'NGC 3585', 'NGC 4478', 'NGC 7507', 'NGC 1637', 'NGC 4473', 'NGC 1365', 'MESSIER 074', 'NGC 4476', 'NGC 4570', 'NGC 5576', 'NGC 4321', 'NGC 5474', 'NGC 7090', 'MESSIER 094', 'MESSIER 095', 'NGC 4494', 'NGC 0247', 'NGC 4490', 'IC 1613', 'NGC 4477', 'NGC 4365', 'NGC 2787', 'NGC 3557', 'IC 5267', 'NGC 4388', 'NGC 3923', 'NGC 4945', 'NGC 891', 'NGC 1300', 'UGC 05340', 'NGC 3631', 'UGCA 166', 'NGC 4314', 'NGC 4550', 'Holmberg IX                   ', 'NGC 4559', 'IC 1459', 'NGC 1399', 'NGC 4039', 'NGC 4038', 'NGC 1316', 'NGC 1097', 'NGC 0383', 'NGC 2681', 'NGC 5018', 'NGC 5253', 'NGC 4631', 'NGC 4308', 'MESSIER 060', 'NGC 4742', 'NGC 1672', 'NGC 5846', 'NGC 4725', 'NGC 2403', 'NGC 3507', 'MESSIER 087', 'NGC 0891', 'NGC 4698', 'NGC 3384', 'NGC 6946', 'NGC 1291:[LFF2012] 084', 'NGC 3115', 'NGC 1332', 'NGC 1700', 'NGC 5584', 'NGC 4527', 'NGC 7552', 'NGC 2997', 'NGC 4449', 'MESSIER 049', 'NGC 3198', 'NGC 0855', 'NGC 7793', 'NGC 0119', 'NGC 2865', 'Circinus Galaxy               ', 'MESSIER 059', 'NGC 7331', 'NGC 1427', 'NGC 3628', 'NGC 3608', 'NGC 0055', 'NGC 4457', 'NGC 4214', 'NGC 4459', 'NGC 3521','NGC 4565', 'NGC 1313', 'NGC 0253'])
+
+#Observation_Tester(['NGC 4278', 'NGC 2841', 'NGC 3877', 'NGC 5194', 'NGC 5054', 'NGC 5813', 'MESSIER 108', 'MESSIER 066', 'MESSIER 061', 'MESSIER 063', 'MESSIER 086', 'MESSIER 084', 'MESSIER 083', 'MESSIER 082', 'NGC 0278', 'MESSIER 088', 'NGC 3585', 'NGC 7507', 'NGC 1637', 'NGC 4473', 'NGC 1365', 'MESSIER 074', 'NGC 4570', 'NGC 5576', 'NGC 4321', 'NGC 5474', 'NGC 7090', 'MESSIER 094', 'MESSIER 095', 'NGC 4494', 'IC 1613', 'NGC 4477', 'NGC 4365', 'NGC 2787', 'NGC 3557', 'IC 5267', 'NGC 4388', 'NGC 3923', 'NGC 891', 'NGC 1300', 'UGC 05340', 'NGC 3631', 'UGCA 166', 'NGC 4314', 'NGC 4550', 'Holmberg IX                   ', 'NGC 4559', 'NGC 1399', 'NGC 1316', 'NGC 1097', 'NGC 2681', 'NGC 5018', 'NGC 5253', 'NGC 4631', 'MESSIER 060', 'NGC 4742', 'NGC 1672', 'NGC 5846', 'NGC 4725', 'NGC 3507', 'MESSIER 087', 'NGC 0891', 'NGC 3384', 'NGC 6946', 'NGC 1291:[LFF2012] 084', 'NGC 3115', 'NGC 1332', 'NGC 1700', 'NGC 5584', 'NGC 7552', 'NGC 2997', 'NGC 4449', 'MESSIER 049', 'NGC 3198', 'NGC 0855', 'NGC 7793', 'NGC 0119', 'NGC 2865', 'MESSIER 059', 'NGC 1427', 'NGC 3628', 'NGC 3608', 'NGC 0055', 'NGC 4457', 'NGC 4214', 'NGC 4459', 'NGC 3521', 'NGC 4565', 'NGC 1313'],Simple_Reg_B=False, Max_Exp_B=True)
+#Observation_Tester(["NGC 1313"])
+#Observation_Tester(["MESSIER_083"])
+#Observation_Tester(["NGC 3608"])
+#Observation_Tester(["NGC 4457","NGC 5813","NGC 3631"])
+#Observation_Tester(['NGC 4278', 'NGC 5194', 'NGC 5054', 'NGC 5813', 'MESSIER 061', 'MESSIER 086', 'MESSIER 084', 'NGC 7507', 'NGC 4473', 'NGC 5576', 'NGC 4321', 'NGC 4477', 'NGC 4365', 'NGC 3557', 'NGC 4388', 'NGC 4314', 'NGC 4550', 'Holmberg IX                   ', 'NGC 1399', 'NGC 1316', 'NGC 1097', 'NGC 2681', 'NGC 5018', 'NGC 4631', 'MESSIER 060', 'NGC 5846', 'NGC 3507', 'MESSIER 087', 'NGC 3384', 'NGC 6946', 'NGC 3115', 'NGC 1332', 'NGC 1700', 'MESSIER 059', 'NGC 3608', 'NGC 4459', 'NGC 4565'],Max_Exp_B=True)
+#NGC 5813
+#Observation_Tester(["NGC 5813"])
+#NGC 4473
+#Observation_Tester(["NGC 4473"])
+#NGC_4321
+#Observation_Tester(["NGC_4321"])
+#Holmberg IX
+#Observation_Tester(["Holmberg IX"])
+#NGC 4631
+#Observation_Tester(["NGC 4631"])
+#NGC 3507
+#Observation_Tester(["NGC 3507"])
+#NGC 3608
+#Observation_Tester(["NGC 3608"])
+#Observation_Tester(["NGC 1313"])
