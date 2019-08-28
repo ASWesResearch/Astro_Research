c46f50d6502b251d951b07234989a01c50591c68
Anthony Santini <asantini@wesleyan.edu>
2019-08-28 17:00:10 -0400

    Reenabled the Try/Except on Subpipe C and Modifed function calls

--- Master_Code/Master_Code.py
+++ Master_Code/Master_Code.py
@@ -319,140 +319,143 @@ def Pipeline_C(Gname_L):
     Galaxy_No_Obs_C_L=[]
     for Gname in Gname_L:
         #Pipeline_C Code
-        ##try:
-        ##print "Gname C: ", Gname
-        """
-        Gname_List=Gname.split(" ")
-        print "Gname_List: ", Gname_List
-        if(len(Gname_List)>1):
-            Gname_Modifed=Gname_List[0]+"_"+Gname_List[1] #Adds underscore to remove space from "NGC #" to change to "NGC_#" if there is a space in the name
-        else:
-            Gname_Modifed=Gname # Does nothing if the galaxy name has no space, ie. NGC#, For example NGC253 instead of NGC 253 or NGC_253
-        """
-        #print "PWD C 2 : "
-        #system('pwd')
-        Evt2_File_H_L=File_Query_Code_5.File_Query(Gname,"evt2")
-        if(Evt2_File_H_L==False):
-            print "Invalid Galaxy"
-            Galaxy_No_Obs_C_L.append(Gname)
-            continue
-        Gname_Modifed=Galaxy_Name_Reducer.Galaxy_Name_Reducer(Gname)
-        #print "Gname_Modifed ", Gname_Modifed
-        #print "PWD C 3 : "
-        #system('pwd')
-        path_2=os.path.realpath('../Master_Code/Master_Output/')
-        #print "path_2 : ", path_2
-        path_3=path_2+'/'+Gname_Modifed+'/'
-        #print "path_3 : ", path_3
-        directory = os.path.dirname(path_3)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        #os.chdir(path_3) #Goes to Current Galaxies Folder
-        path_Flux_90=path_3+'Flux_90_Files/'
-        #print "path_Flux_90 : ", path_Flux_90
-        directory_Flux_90=os.path.dirname(path_Flux_90)
-        if not os.path.exists(directory_Flux_90):
-            os.makedirs(directory_Flux_90)
-        #print "path_Flux_90=",path_Flux_90
-        #os.chdir(path_Flux_90)
-        Evt2_File_H_L=File_Query_Code_5.File_Query(Gname,"evt2")
-        Reg_File_H_L=File_Query_Code_5.File_Query(Gname,"reg",".reg")
-        #print "Evt2_File_H_L ", Evt2_File_H_L
-        #print "Reg_File_H_L ", Reg_File_H_L
-        for Evt2_File_L in Evt2_File_H_L:
-            Cur_Evt2_ObsID=Evt2_File_L[0]
-            Cur_Evt2_Filepath=Evt2_File_L[1]
-            for Reg_File_L in Reg_File_H_L:
-                Cur_Reg_ObsID=Reg_File_L[0]
-                Cur_Reg_Filepath=Reg_File_L[1]
-                if(Cur_Evt2_ObsID==Cur_Reg_ObsID):
-                    print "Cur_Evt2_ObsID : ", Cur_Evt2_ObsID
-                    #print "Test"
-                    #path_Obs=path_Flux_90+'/'+str(Cur_Evt2_ObsID)+'/'
-                    #print "Cur_Evt2_ObsID : ",Cur_Evt2_ObsID
-                    #print "type(Cur_Evt2_ObsID) : ",type(Cur_Evt2_ObsID)
-                    #print "Cur_Evt2_Filepath : ", Cur_Evt2_Filepath
-                    #Cur_Evt2_Filepath_L=Cur_Evt2_Filepath.split("/")
-                    #print "Cur_Evt2_Filepath_L : ",Cur_Evt2_Filepath_L
-                    #Cur_Evt2_Filename=Cur_Evt2_Filepath_L[-1]
-                    #print "Cur_Evt2_Filename : ", Cur_Evt2_Filename
-                    path_Obs=path_Flux_90+str(Cur_Evt2_ObsID)+'/'
-                    directory_Obs=os.path.dirname(path_Obs)
-                    if not os.path.exists(directory_Obs):
-                        os.makedirs(directory_Obs)
-                    #print "path_Obs : ", path_Obs
-                    #os.chdir(path_Obs)
-                    #Cur_Evt2_Filepath_Full=path_Obs+Cur_Evt2_Filename
-                    #print "path_Obs : ", path_Obs
-                    #print "Cur_Evt2_Filepath_Full : ", Cur_Evt2_Filepath_Full
-                    #print "THE PWD AT END IS :"
-                    #system('pwd')
-                    Source_Region_Generator_Radius_Modifed_V3.Source_Region_Generator_Radius_Modifed_V3(Gname,path_Obs)
-                    if(Gname[3]==" "):
-                        Gname_Underscore=Gname.replace(Gname[3], "_", 1)
-                    elif(Gname[3]!="_"):
-                        Gname_Underscore_L=Gname.split(Gname[2])
-                        #print "Gname_Underscore_L : ",Gname_Underscore_L
-                        Gname_Underscore=Gname_Underscore_L[0]+Gname[2]+"_"+Gname_Underscore_L[1]
-                    #print "Gname_Underscore Master : ", Gname_Underscore
-                    #NGC_4258_ObsID_1618_Source_Regions_Radius_Modifed.txt
-                    ObjF_Filename=Gname_Underscore+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Source_Regions_Radius_Modifed.txt"
-                    ObjF_Filepath=path_Obs+ObjF_Filename
-                    #print "ObjF_Filepath : ", ObjF_Filepath
-                    BG_Radius=50
-                    while (BG_Radius>20):
-                        #Background_Finder_3(gname,evtfpath,objLfname,R)
-                        Background_Ratio=Background_Finder_10.Background_Finder_3(Gname,Cur_Evt2_Filepath,ObjF_Filepath,BG_Radius)
-                        if(Background_Ratio != "None_Found"):
-                            break
-                        BG_Radius=BG_Radius-10
-                    #print "Background_Ratio : ", Background_Ratio
-                    Background_Ratio_L=[Background_Ratio]
-                    #print "Background_Ratio_L: ", Background_Ratio_L
-                    #D_P_C_Big_Input_90_Per_Check(Backgrounds,Off_Angs,C_Min=2,C_Max=110)
-                    #print "THE PWD AT END IS :"
-                    #system('pwd')
-                    #path_4=os.path.realpath('../../../../')
-                    #print "path_4 : ",path_4
-                    #os.chdir(path_4)
-                    #print "Output : ",Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
-                    #system('pwd')
-                    #C_90_Per_First_L_H=Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
-                    try: #This "Try-Except" with be replaced with the Obs_ID_Checker module
-                        C_90_Per_First_L_H=Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
-                    except:
-                        ObsID_Fail_C_L.append([Gname,Cur_Evt2_ObsID])
-                        continue
-                    #print "C_90_Per_First_L_H : ", C_90_Per_First_L_H
-                    #os.chdir(path_Obs)
-                    #print "THE PWD AT END IS :"
-                    #system('pwd')
-                    #Known_Flux_Finder('NGC2403','acisf02014_repro_evt2.fits')
-                    Known_Flux=Known_Flux_Finder.Known_Flux_Finder(Gname,Cur_Evt2_Filepath)
-                    #print "Known_Flux : ",Known_Flux
-                    Known_Flux_L=[Known_Flux]
-                    #print "Known_Flux_L : ",Known_Flux_L
-                    Flux_90_L_H=Counts_To_Flux_Converter_3.Counts_To_Flux_Converter(C_90_Per_First_L_H,Known_Flux_L)
-                    #print "Flux_90_L_H : ", Flux_90_L_H
-                    #print "THE PWD AT END IS :"
-                    #system('pwd')
-                    #os.chdir(path_Obs)
-                    #print "THE PWD AT END IS :"
-                    #system('pwd')
-                    Flux_90_L=Flux_90_L_H[0]
-                    #print "Flux_90_L : ", Flux_90_L
-                    #NGC_4258_ObsID_1618
-                    file=open(path_Obs+Gname_Modifed+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Flux_90.txt","w")
-                    file.write("Background_Ratio:"+str(Background_Ratio)+"|"+"\n")
-                    for Flux_90 in Flux_90_L:
-                        file.write(str(Flux_90)+"\n")
-        #path_5=os.path.realpath('../../../../')
-        #print "path_5 : ",path_5
-        #os.chdir(path_5)
-        #print "THE PWD AT VERY END IS :"
-        #system('pwd')
-        ##except:
-            ##Galaxy_Fail_C_L.append(Gname)
+        try:
+            print "Gname C: ", Gname
+            """
+            Gname_List=Gname.split(" ")
+            print "Gname_List: ", Gname_List
+            if(len(Gname_List)>1):
+                Gname_Modifed=Gname_List[0]+"_"+Gname_List[1] #Adds underscore to remove space from "NGC #" to change to "NGC_#" if there is a space in the name
+            else:
+                Gname_Modifed=Gname # Does nothing if the galaxy name has no space, ie. NGC#, For example NGC253 instead of NGC 253 or NGC_253
+            """
+            #print "PWD C 2 : "
+            #system('pwd')
+            Evt2_File_H_L=File_Query_Code_5.File_Query(Gname,"evt2")
+            if(Evt2_File_H_L==False):
+                print "Invalid Galaxy"
+                Galaxy_No_Obs_C_L.append(Gname)
+                continue
+            Gname_Modifed=Galaxy_Name_Reducer.Galaxy_Name_Reducer(Gname)
+            #print "Gname_Modifed ", Gname_Modifed
+            #print "PWD C 3 : "
+            #system('pwd')
+            path_2=os.path.realpath('../Master_Code/Master_Output/')
+            #print "path_2 : ", path_2
+            path_3=path_2+'/'+Gname_Modifed+'/'
+            #print "path_3 : ", path_3
+            directory = os.path.dirname(path_3)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            #os.chdir(path_3) #Goes to Current Galaxies Folder
+            path_Flux_90=path_3+'Flux_90_Files/'
+            #print "path_Flux_90 : ", path_Flux_90
+            directory_Flux_90=os.path.dirname(path_Flux_90)
+            if not os.path.exists(directory_Flux_90):
+                os.makedirs(directory_Flux_90)
+            #print "path_Flux_90=",path_Flux_90
+            #os.chdir(path_Flux_90)
+            Evt2_File_H_L=File_Query_Code_5.File_Query(Gname,"evt2")
+            Reg_File_H_L=File_Query_Code_5.File_Query(Gname,"reg",".reg")
+            #print "Evt2_File_H_L ", Evt2_File_H_L
+            #print "Reg_File_H_L ", Reg_File_H_L
+            for Evt2_File_L in Evt2_File_H_L:
+                Cur_Evt2_ObsID=Evt2_File_L[0]
+                Cur_Evt2_Filepath=Evt2_File_L[1]
+                for Reg_File_L in Reg_File_H_L:
+                    Cur_Reg_ObsID=Reg_File_L[0]
+                    Cur_Reg_Filepath=Reg_File_L[1]
+                    if(Cur_Evt2_ObsID==Cur_Reg_ObsID):
+                        print "Cur_Evt2_ObsID : ", Cur_Evt2_ObsID
+                        #print "Test"
+                        #path_Obs=path_Flux_90+'/'+str(Cur_Evt2_ObsID)+'/'
+                        #print "Cur_Evt2_ObsID : ",Cur_Evt2_ObsID
+                        #print "type(Cur_Evt2_ObsID) : ",type(Cur_Evt2_ObsID)
+                        #print "Cur_Evt2_Filepath : ", Cur_Evt2_Filepath
+                        #Cur_Evt2_Filepath_L=Cur_Evt2_Filepath.split("/")
+                        #print "Cur_Evt2_Filepath_L : ",Cur_Evt2_Filepath_L
+                        #Cur_Evt2_Filename=Cur_Evt2_Filepath_L[-1]
+                        #print "Cur_Evt2_Filename : ", Cur_Evt2_Filename
+                        path_Obs=path_Flux_90+str(Cur_Evt2_ObsID)+'/'
+                        directory_Obs=os.path.dirname(path_Obs)
+                        if not os.path.exists(directory_Obs):
+                            os.makedirs(directory_Obs)
+                        #print "path_Obs : ", path_Obs
+                        #os.chdir(path_Obs)
+                        #Cur_Evt2_Filepath_Full=path_Obs+Cur_Evt2_Filename
+                        #print "path_Obs : ", path_Obs
+                        #print "Cur_Evt2_Filepath_Full : ", Cur_Evt2_Filepath_Full
+                        #print "THE PWD AT END IS :"
+                        #system('pwd')
+                        Source_Region_Generator_Radius_Modifed_V3.Source_Region_Generator_Radius_Modifed_V3(Gname,path_Obs)
+                        """
+                        if(Gname[3]==" "):
+                            Gname_Underscore=Gname.replace(Gname[3], "_", 1)
+                        elif(Gname[3]!="_"):
+                            Gname_Underscore_L=Gname.split(Gname[2])
+                            #print "Gname_Underscore_L : ",Gname_Underscore_L
+                            Gname_Underscore=Gname_Underscore_L[0]+Gname[2]+"_"+Gname_Underscore_L[1]
+                        #print "Gname_Underscore Master : ", Gname_Underscore
+                        """
+                        #NGC_4258_ObsID_1618_Source_Regions_Radius_Modifed.txt
+                        #ObjF_Filename=Gname_Underscore+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Source_Regions_Radius_Modifed.txt"
+                        ObjF_Filename=Gname_Modifed+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Source_Regions_Radius_Modifed.txt"
+                        ObjF_Filepath=path_Obs+ObjF_Filename
+                        #print "ObjF_Filepath : ", ObjF_Filepath
+                        BG_Radius=50
+                        while (BG_Radius>20):
+                            #Background_Finder_3(gname,evtfpath,objLfname,R)
+                            Background_Ratio=Background_Finder_10.Background_Finder_3(Gname,Cur_Evt2_Filepath,ObjF_Filepath,BG_Radius)
+                            if(Background_Ratio != "None_Found"):
+                                break
+                            BG_Radius=BG_Radius-10
+                        #print "Background_Ratio : ", Background_Ratio
+                        Background_Ratio_L=[Background_Ratio]
+                        #print "Background_Ratio_L: ", Background_Ratio_L
+                        #D_P_C_Big_Input_90_Per_Check(Backgrounds,Off_Angs,C_Min=2,C_Max=110)
+                        #print "THE PWD AT END IS :"
+                        #system('pwd')
+                        #path_4=os.path.realpath('../../../../')
+                        #print "path_4 : ",path_4
+                        #os.chdir(path_4)
+                        #print "Output : ",Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
+                        #system('pwd')
+                        #C_90_Per_First_L_H=Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
+                        try: #This "Try-Except" with be replaced with the Obs_ID_Checker module
+                            C_90_Per_First_L_H=Detection_Probability_Calc_7.D_P_C_Big_Input_90_Per_Check(Background_Ratio_L)
+                        except:
+                            ObsID_Fail_C_L.append([Gname,Cur_Evt2_ObsID])
+                            continue
+                        #print "C_90_Per_First_L_H : ", C_90_Per_First_L_H
+                        #os.chdir(path_Obs)
+                        #print "THE PWD AT END IS :"
+                        #system('pwd')
+                        #Known_Flux_Finder('NGC2403','acisf02014_repro_evt2.fits')
+                        Known_Flux=Known_Flux_Finder.Known_Flux_Finder(Gname,Cur_Evt2_Filepath)
+                        #print "Known_Flux : ",Known_Flux
+                        Known_Flux_L=[Known_Flux]
+                        #print "Known_Flux_L : ",Known_Flux_L
+                        Flux_90_L_H=Counts_To_Flux_Converter_3.Counts_To_Flux_Converter(C_90_Per_First_L_H,Known_Flux_L)
+                        #print "Flux_90_L_H : ", Flux_90_L_H
+                        #print "THE PWD AT END IS :"
+                        #system('pwd')
+                        #os.chdir(path_Obs)
+                        #print "THE PWD AT END IS :"
+                        #system('pwd')
+                        Flux_90_L=Flux_90_L_H[0]
+                        #print "Flux_90_L : ", Flux_90_L
+                        #NGC_4258_ObsID_1618
+                        file=open(path_Obs+Gname_Modifed+"_ObsID_"+str(Cur_Evt2_ObsID)+"_Flux_90.txt","w")
+                        file.write("Background_Ratio:"+str(Background_Ratio)+"|"+"\n")
+                        for Flux_90 in Flux_90_L:
+                            file.write(str(Flux_90)+"\n")
+                #path_5=os.path.realpath('../../../../')
+                #print "path_5 : ",path_5
+                #os.chdir(path_5)
+                #print "THE PWD AT VERY END IS :"
+                #system('pwd')
+        except:
+            Galaxy_Fail_C_L.append(Gname)
 
     print "Galaxy_Fail_C_L : ", Galaxy_Fail_C_L
     print "Galaxy_No_Obs_C_L : ", Galaxy_No_Obs_C_L
@@ -593,3 +596,7 @@ if __name__ == '__main__':
     #Master(['NGC 0253']) #Small Sample of Galaxies for testing
     Master(['NGC 3631']) #Small Sample of Galaxies for testing
     #Master(['NGC 2841', 'NGC 3877', 'NGC 5054', 'NGC 5813', 'MESSIER 108', 'MESSIER 066', 'MESSIER 061', 'MESSIER 063', 'MESSIER 086', 'MESSIER 084', 'MESSIER 083', 'MESSIER 082', 'NGC 0278', 'MESSIER 088', 'NGC 3585', 'NGC 7507', 'NGC 1637', 'NGC 4473', 'NGC 1365', 'MESSIER 074', 'NGC 4570', 'NGC 4321', 'NGC 5474', 'NGC 7090', 'MESSIER 094', 'MESSIER 095', 'NGC 4494', 'IC 1613', 'NGC 4477', 'NGC 2787', 'IC 5267', 'NGC 3923', 'NGC 891', 'NGC 1300', 'UGC 05340', 'NGC 3631', 'NGC 4314', 'NGC 4559', 'NGC 2681', 'NGC 5018', 'NGC 5253', 'NGC 4742', 'NGC 1672', 'NGC 4725', 'NGC 0891', 'NGC 6946', 'NGC 1291:[LFF2012] 084', 'NGC 3115', 'NGC 1332', 'NGC 1700', 'NGC 5584', 'NGC 7552', 'NGC 2997', 'NGC 4449', 'MESSIER 049', 'NGC 3198', 'NGC 0855', 'NGC 7793', 'NGC 2865', 'MESSIER 059', 'NGC 1427', 'NGC 3628', 'NGC 4457', 'NGC 4214', 'NGC 4459', 'NGC 3521']) #This is the current correct Master Code Input Galaxy Name List, The Master Code is Ready to Run ! ! !
+    #Master(['MESSIER 063','MESSIER 084','NGC 1365','NGC 4559','NGC 5018','MESSIER 049']) #Small Sample of Galaxies for testing
+    #Master(['MESSIER 049']) #Small Sample of Galaxies for testing
+    #Master(['NGC 4559']) #Small Sample of Galaxies for testing
+    #Master(['NGC 1365']) #Small Sample of Galaxies for testing
