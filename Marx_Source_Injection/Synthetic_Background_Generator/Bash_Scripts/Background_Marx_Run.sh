5eac52073ec3f0702b0db60be20670c2e1edc0ce
Anthony Santini <asantini@mando.astro.wesleyan.edu>
2024-10-04 01:48:41 -0400

    Modified Marx_Source_Injection/Synthetic_Background_Generator/Bash_Scripts/Background_Marx_Run.sh into a working state

--- Marx_Source_Injection/Synthetic_Background_Generator/Bash_Scripts/Background_Marx_Run.sh
+++ Marx_Source_Injection/Synthetic_Background_Generator/Bash_Scripts/Background_Marx_Run.sh
@@ -1,10 +1,19 @@
 #dmcopy "acis7sbkg.fits[EVENTS][bin x=::32,y=::32]" chip-shape.fits option=image
-dmcopy "$1[EVENTS][bin x=::32,y=::32]" chip-shape.fits option=image
-marx @@/opt/anaconda3/envs/ciao-4.14/share/marx/pfiles/marx.par SourceFlux=3. SpectrumType="FILE" SpectrumFile="bkgspec.tbl" \
-       ExposureTime=0 NumRays=$2 TStart=2012.5 OutputDir=$6 \
-       DetectorType="ACIS-S" DitherModel="INTERNAL" RA_Nom=0 Dec_Nom=0 \
-       Roll_Nom=0 SourceRA=$3 SourceDEC=$4 \
-       SourceType="IMAGE" S-ImageFile="chip-shape.fits"
+#dmcopy $1"[EVENTS][bin x=::32,y=::32]" $6_chip-shape.fits option=image
+#$5"_bkgspec.tbl"
+#dmcopy $5"_acis5s_bkg_reproj.fits[EVENTS][bin x=::32,y=::32]" $5_chip-shape.fits option=image
+##dmcopy $7"[sky=rotbox(0:00:58,+0:00:10,8.5',8.5',0)]" $5_image.fits
+#dmcopy $7"[EVENTS][bin x=::32,y=::32]" $5_chip-shape.fits option=image
+##dmcopy $5_image.fits"[EVENTS][bin x=::32,y=::32]" $5_chip-shape.fits option=image
+marx @@$6 SourceFlux=3. SpectrumType="FILE" SpectrumFile=../Background_Generator/Test_Example/bkgspec.tbl \
+       ExposureTime=0 NumRays=-$1 OutputDir=$5 \
+       DetectorType="ACIS-I" DitherModel="INTERNAL" RA_Nom=$2 Dec_Nom=$3 \
+       Roll_Nom=0 SourceRA=$2 SourceDEC=$3 \
+       SourceType="IMAGE" S-ImageFile=../Background_Generator/Test_Example/chip-shape.fits GratingType=NONE
+#$5"_chip-shape.fits"
+#TStart=2012.5
 #marxcat diffuse bkg diffuse_with_bkg
-marxcat $5 $6 source_with_bkg
-marx2fits source_with_bkg source_with_bkg.fits
+#marxcat $4 $5 $5_source_with_bkg
+marxcat $5 $4 $5_source_with_bkg
+marx2fits $5_source_with_bkg $5_source_with_bkg.fits
+marx2fits $5 $5.fits
